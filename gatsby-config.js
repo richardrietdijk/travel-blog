6b79f8b7eee9e7ae656a9421daf703a289a3a44f
Richard Rietdijk <richardrietdijk@gmail.com>
2020-07-10 10:28:53 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -6,6 +10,20 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `ikoa2tzwge9w`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`baskerville\:400`, `muli\:300,700`],
+      },
+    },
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
